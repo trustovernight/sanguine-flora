beb7cd663915b45b168e7961a8649442b37af024
trustovernight <vls445154@gmail.com>
2026-05-02 12:53:12 +0200

    Add initial mod

---
+++ src/main/java/trustovernight/sanguineflora/SanguineFlora.java
@@ -0,0 +1,13 @@
+package trustovernight.sanguineflora;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SanguineFlora.MODID)
+public class SanguineFlora {
+    public static final String MODID = "sanguineflora";
+
+    public SanguineFlora(IEventBus modEventBus) {
+
+    }
+}
